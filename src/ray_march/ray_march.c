2d2604c6938f3161c78d3aaa042165f5f85a20ba
colourblind <tom.milsom@gmail.com>
2013-01-12 20:35:47 +0000

    Still pants

--- src/ray_march/ray_march.c
+++ src/ray_march/ray_march.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 
-const float march_limit = 50; // Distance before we give up
-const float march_step_broad = 0.05f; // Depth increment during broad phase testing
-const float march_step = 0.0000005f; // Depth increment in each iteration
-const float fov = 90; // Horizontal field of view
+const double march_limit = 40; // Distance before we give up
+const double march_step = 0.0000001f; // Depth increment in each iteration
+const double march_step_broad = 0.01f;
+const double fov = 90; // Horizontal field of view
 
-#define DEGS_TO_RADS(t)		((t) / 180.0f * 3.141592654f)
+#define DEGS_TO_RADS(t)		((t) / 180.0 * 3.141592654)
 #define CLAMP(t, mint, maxt)	((t) < (mint) ? (mint) : ((t) > (maxt) ? (maxt) : (t)))
-#define ABSF(f)		((f) < 0 ? (f) * -1 : (f))
+#define ABS(f)		((f) < 0 ? (f) * -1 : (f))
 #define MIN(x, y)   ((x) < (y) ? (x) : (y))
 #define MAX(x, y)   ((x) > (y) ? (x) : (y))
 
@@ -17,7 +17,7 @@ int save_png(char *filename, unsigned char *data, int width, int height);
 
 typedef struct vec3
 {
-    float x, y, z;
+    double x, y, z;
 } vec3;
 
 vec3 vec_add(vec3 a, vec3 b)
@@ -29,7 +29,7 @@ vec3 vec_add(vec3 a, vec3 b)
     return r;
 }
 
-vec3 vec_mult(vec3 a, float b)
+vec3 vec_mult(vec3 a, double b)
 {
     vec3 r;
     r.x = a.x * b;
@@ -38,25 +38,31 @@ vec3 vec_mult(vec3 a, float b)
     return r;
 }
 
-float vec_length(vec3 a)
+double vec_length_sq(vec3 a)
 {
-	return sqrtf(a.x * a.x + a.y * a.y + a.z * a.z);
+    return a.x * a.x + a.y * a.y + a.z * a.z;
+}
+
+double vec_length(vec3 a)
+{
+	return sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
 }
 
 vec3 vec_norm(vec3 a)
 {
     vec3 r;
-    float l = vec_length(a);
+    double l = vec_length(a);
     r.x = a.x / l;
     r.y = a.y / l;
     r.z = a.z / l;
     return r;
 }
 
-void save(float **data, int width, int height, float min_depth, float max_depth)
+void save(double **data, int width, int height, double min_depth, double max_depth)
 {
 	int i, j;
 	unsigned char *d = malloc(width * height * sizeof(unsigned char));
+    double fog;
 
 	memset(d, 0, width * height * sizeof(unsigned char));
 	for (i = 0; i < width; i ++)
@@ -65,8 +71,9 @@ void save(float **data, int width, int height, float min_depth, float max_depth)
 		{
 			if (data[i][j] >= 0)
 			{
-				float fog = (CLAMP(data[i][j], min_depth, max_depth) - min_depth) / (max_depth - min_depth);
+				fog = (CLAMP(data[i][j], min_depth, max_depth) - min_depth) / (max_depth - min_depth);
 				d[j * width + i] = (unsigned char)((1 - fog) * 255);
+				//d[j * width + i] = 255;
 			}
 		}
 	}
@@ -80,24 +87,24 @@ void go(int width, int height, int (*objectFunc)(vec3))
 {
     int i;
     int x, y;
-    float **depth;
-    float march;
-    float half_fov_h = DEGS_TO_RADS(fov / 2);
-    float half_fov_v = DEGS_TO_RADS((fov / 2) * ((float)height / width));
-    float half_width = (float)width / 2;
-    float half_height = (float)height / 2;
+    double **depth;
+    double march;
+    double half_fov_h = DEGS_TO_RADS(fov / 2);
+    double half_fov_v = DEGS_TO_RADS((fov / 2) * ((double)height / width));
+    double half_width = (double)width / 2;
+    double half_height = (double)height / 2;
     vec3 camera_pos, pos, dir;
-	float min_depth = march_limit, max_depth = 0;
+	double min_depth = march_limit, max_depth = 0;
     int test_result;
 
     // Init depth array
-    depth = (float **)malloc(width * sizeof(float));
+    depth = (double **)malloc(width * sizeof(double));
     for (i = 0; i < width; i ++)
-        depth[i] = (float *)malloc(height * sizeof(float));
+        depth[i] = (double *)malloc(height * sizeof(double));
 
 	camera_pos.x = 0;
 	camera_pos.y = 0;
-	camera_pos.z = -10;
+	camera_pos.z = -16;
         
     // Iterate over pixels
     for (x = 0; x < width; x ++)
@@ -106,28 +113,28 @@ void go(int width, int height, int (*objectFunc)(vec3))
         {
             depth[x][y] = -1;
             // Generate dir vector
-            dir.x = sinf(((x - half_width) / width) * half_fov_h);
-            dir.y = sinf(((y - half_height) / height) * half_fov_v);
-            dir.z = cosf(((x - half_width) / width) * half_fov_h);
+            dir.x = sin(((x - half_width) / width) * half_fov_h);
+            dir.y = sin(((y - half_height) / height) * half_fov_v);
+            dir.z = cos(((x - half_width) / width) * half_fov_h);
             dir = vec_norm(dir); // necessary?
             // Ten hut!
-            march = 0;
+            march = 8; // cheat!
             while (march < march_limit)
             {
                 pos = vec_add(camera_pos, vec_mult(dir, march));
                 test_result = objectFunc(pos);
 
-                if (objectFunc(pos) > 0)
+                if (test_result > 0)
                 {
                     depth[x][y] = march;
                     min_depth = MIN(min_depth, march);
                     max_depth = MAX(max_depth, march);
                     break;
                 }
-                else if (test_result == 0) // small steps
-                    march += march_step;
-                else // miles out. Large steps
+                else if (test_result < 0)
                     march += march_step_broad;
+                else
+                    march += march_step;
             }
         }
         printf(".");
@@ -141,44 +148,46 @@ void go(int width, int height, int (*objectFunc)(vec3))
     free(depth);
 }
 
-float test_object(vec3 v)
+int test_object(vec3 v)
 {
-    if (v.x > 1 && v.x < 3 && v.y > 1 && v.y < 3 && v.z > -1 && v.z < 1)
+    if (v.x > -1 && v.x < 1 && v.y > -1 && v.y < 1 && v.z > -1 && v.z < 1)
         return 1;
     else
         return -1;
 }
 
-vec3 fold(vec3 v)
+vec3 iterate(vec3 v, vec3 c)
 {
-    float r = 0.5f;
-    float f = 1;
-	float mag;
+    double scale = 2;
+    double fixed_radius = (1 * 1);
+    double min_radius = (0.5 * 0.5);
+	double mag;
 
     // Box folds for each component
 	if (v.x > 1)
-		v.x = 2 - v.x;
+		v.x = scale - v.x;
 	else if (v.x < -1)
-		v.x = -2 - v.x;
+		v.x = -scale - v.x;
 
 	if (v.y > 1)
-		v.y = 2 - v.y;
+		v.y = scale - v.y;
 	else if (v.y < -1)
-		v.y = -2 - v.y;
+		v.y = -scale - v.y;
 
 	if (v.z > 1)
-		v.z = 2 - v.z;
+		v.z = scale - v.z;
 	else if (v.z < -1)
-		v.z = -2 - v.z;
-
-    v = vec_mult(v, f);
+		v.z = -scale - v.z;
 
     // Sphere fold
 	mag = vec_length(v);
-	if (mag < r)
-		v = vec_mult(v, 1.f / (r * r));
-	else if (mag < 1) 
-		v = vec_mult(v, 1.f / mag);
+	if (mag < min_radius)
+		v = vec_mult(v, (fixed_radius * fixed_radius) / (min_radius * min_radius));
+	else if (mag < fixed_radius) 
+		v = vec_mult(v, (fixed_radius * fixed_radius) / (mag * mag));
+
+    v = vec_mult(v, scale);
+    v = vec_add(v, c);
 
 	return v;
 }
@@ -186,29 +195,30 @@ vec3 fold(vec3 v)
 int mandelbox(vec3 c)
 {
     int attempts;
-    int attempt_limit = 10000;
-    float scale = 2;
-    float bounds = 6;
+    int attempt_limit = 1000;
+    double bounds = 2;
     vec3 p;
+    double spacing = bounds + march_step_broad + march_step;
 
-    if (ABSF(c.x) > bounds + march_step_broad || ABSF(c.y) > + march_step_broad || ABSF(c.z) > + march_step_broad)
+    if (ABS(c.x) > spacing || ABS(c.y) > spacing || ABS(c.z) > spacing)
         return -1;
 
-	p.x = p.y = p.z = 0;
+	p.x = c.x;
+    p.y = c.y;
+    p.z = c.z;
 	for (attempts = 0; attempts < attempt_limit; attempts ++)
 	{
-		p = vec_add(vec_mult(fold(p), scale), c);
-		if (ABSF(p.x) > bounds || ABSF(p.y) > bounds || ABSF(p.z) > bounds)
-			break;
+		p = iterate(p, c);
+		//if (vec_length_sq(p) > 121)
+        if (ABS(p.x) > spacing || ABS(p.y) > spacing || ABS(p.z) > spacing)
+			return 0;
 	}
 
-    if (attempts == attempt_limit) // Escaped
-        return 0;
-    else
-        return 1;
+    return 1;
 }
 
 int main(int argc, char **argv)
 {
-    go(120, 90, &mandelbox);
+    //go(120, 90, &test_object);
+    go(240, 180, &mandelbox);
 }
