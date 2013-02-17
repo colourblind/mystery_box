13128339d21b9a1d5925972fabadc403f13c027d
colourblind <tom.milsom@gmail.com>
2013-02-17 10:36:29 +0000

    Multi-threaded animation now terminates properly

--- tools/threaded.py
+++ tools/threaded.py
@@ -72,6 +72,6 @@ if __name__ == '__main__':
         thread.daemon = True
         thread.start()
         
-    while True:
-        time.sleep(100)
+    while threading.active_count() > 1:
+        time.sleep(5)
   
