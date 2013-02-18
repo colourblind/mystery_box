25d4bb639ef9e1455b0f31702528b970af642852
colourblind <tom.milsom@gmail.com>
2013-02-18 23:36:22 +0000

    Simplified animation scripts. Added synchronisation to threaded animator

--- tools/anim.py
+++ tools/anim.py
@@ -2,51 +2,42 @@ import os
 import sys
 import json
 import camera_path
+import subprocess
 
-def render(camera, template, config_filename, filename):
-    camera_pos = '{0} {1} {2}'.format(camera[0][0], camera[0][1], camera[0][2])
-    camera_target = '{0} {1} {2}'.format(camera[1][0], camera[1][1], camera[1][2])
-    frame_config = template.replace('{camera_pos}', camera_pos)
-    frame_config = frame_config.replace('{camera_target}', camera_target)
-    frame_config = frame_config.replace('{output_file}', filename)
+def render(config, config_filename, hide_output = False):
+    frame_config = json_to_config(config)
     f = open(config_filename, 'w')
     f.write(frame_config)
-    f.flush()
     f.close()
-    execute_line = os.path.join('.', 'ray_march') + ' ' + config_filename + ' > nul'
-    os.system(execute_line)
+    if hide_output:
+        out = open(os.devnull, 'w')
+    else:
+        out = None
+    subprocess.call([os.path.join('.', 'ray_march'), config_filename], stdout=out)
         
 def go(c):
-    template = config_template
     track = camera_path.Pather(c['key_frames'])
     if not os.path.exists(c['output_dir']):
         os.mkdir(c['output_dir'])
+
     renderer = c['renderer']
-    template = template.replace('{scale}', str(renderer['scale']))
-    template = template.replace('{light_pos}', str(renderer['light_pos']))
-    template = template.replace('{bailout}', str(renderer['bailout']))
-    template = template.replace('{width}', str(renderer['width']))
-    template = template.replace('{height}', str(renderer['height']))
+    
     if len(sys.argv) > 2:
         start = int(sys.argv[2])
     else:
         start = 0
+        
     for i in xrange(start, int(c['key_frames'][-1]['time'] * c['fps'])):
         t = float(i) / c['fps']
         camera = track.get(t)
-        filename = os.path.join(c['output_dir'], str(i))
-        render(camera, template, 'go.cfg', filename)
-
-config_template = """scale: {scale}
-camera_pos: {camera_pos}
-camera_target: {camera_target}
-light_pos: {light_pos}
-output_file: {output_file}.png
-width: {width}
-height: {height}
-bailout: {bailout}
-fov: 90
-"""
+        filename = os.path.join(c['output_dir'], str(i) + '.png')
+        renderer['camera_pos'] = '{0} {1} {2}'.format(camera[0][0], camera[0][1], camera[0][2])
+        renderer['camera_target'] = '{0} {1} {2}'.format(camera[1][0], camera[1][1], camera[1][2])
+        renderer['output_file'] = filename
+        render(renderer, 'go.cfg')
+        
+def json_to_config(data):
+    return '\n'.join(['{0}: {1}'.format(key, data[key]) for key in data])        
 
 if __name__ == '__main__':
     f = open(sys.argv[1], 'r')
--- tools/threaded.py
+++ tools/threaded.py
@@ -8,67 +8,57 @@ import camera_path
 
 THREAD_COUNT = 4
 
-def go(thread_no, c, track, template, dispatcher):
-    # Fetch next frame number
-    frame = dispatcher.get_next_frame()
-    while frame >= 0:
-        t = float(frame) / c['fps']
-        camera = track.get(t)
-        filename = os.path.join(c['output_dir'], str(frame))
-        print('{0}: Starting frame {1}'.format(thread_no, frame))
-        anim.render(camera, template, 'go-{0}.cfg'.format(thread_no), filename)
-        frame = dispatcher.get_next_frame()
+def go(thread_no, dispatcher):
+    frame_no, frame_config = dispatcher.get_next_frame()
+    while frame_no >= 0:
+        print('{0}: Starting frame {1}'.format(thread_no, frame_no))
+        anim.render(frame_config, 'go-{0}.cfg'.format(thread_no), hide_output = True)
+        frame_no, frame_config = dispatcher.get_next_frame()
     
 class Dispatcher(object):
-    def __init__(self, max_frame, first_frame = 0):
-        self.max_frame = max_frame
+    def __init__(self, config, first_frame = 0):
+        self.config = config
+        self.track = camera_path.Pather(config['key_frames'])
+        self.max_frame = int(config['key_frames'][-1]['time'] * config['fps'])
         self.first_frame = first_frame
         self.next_frame = self.first_frame
+        self.lock = threading.Lock()
         
     def get_next_frame(self):
         if self.next_frame >= self.max_frame:
-            return -1
+            return -1, None
         else:
+            self.lock.acquire()
+            frame_no = self.next_frame
             self.next_frame = self.next_frame + 1
-            return self.next_frame - 1
-
-        
-config_template = """scale: {scale}
-camera_pos: {camera_pos}
-camera_target: {camera_target}
-light_pos: {light_pos}
-output_file: {output_file}.png
-width: {width}
-height: {height}
-bailout: {bailout}
-fov: 90
-"""
+            self.lock.release()
+            
+            frame_config = self.config['renderer']
+            t = float(frame_no) / self.config['fps']
+            camera = self.track.get(t)
+            filename = os.path.join(c['output_dir'], str(frame_no) + '.png')
+            frame_config['camera_pos'] = '{0} {1} {2}'.format(camera[0][0], camera[0][1], camera[0][2])
+            frame_config['camera_target'] = '{0} {1} {2}'.format(camera[1][0], camera[1][1], camera[1][2])
+            frame_config['output_file'] = filename
+            
+            return frame_no, frame_config
 
 if __name__ == '__main__':
     f = open(sys.argv[1], 'r')
     c = json.load(f)
     f.close()
     
-    track = camera_path.Pather(c['key_frames'])
-    execute_line = os.path.join('.', 'ray_march') + ' go.cfg'
     if not os.path.exists(c['output_dir']):
         os.mkdir(c['output_dir'])
-    renderer = c['renderer']
-    template = config_template
-    template = template.replace('{scale}', str(renderer['scale']))
-    template = template.replace('{light_pos}', str(renderer['light_pos']))
-    template = template.replace('{bailout}', str(renderer['bailout']))
-    template = template.replace('{width}', str(renderer['width']))
-    template = template.replace('{height}', str(renderer['height']))
     
     if len(sys.argv) > 2:
         start = int(sys.argv[2])
     else:
         start = 0
-    dispatcher = Dispatcher(int(c['key_frames'][-1]['time'] * c['fps']), start)
+    dispatcher = Dispatcher(c, start)
     
     for i in xrange(THREAD_COUNT):
-        thread = threading.Thread(name=str(i), target=go, args=(i, c, track, template, dispatcher,))
+        thread = threading.Thread(name=str(i), target=go, args=(i, dispatcher,))
         thread.daemon = True
         thread.start()
         
